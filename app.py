6fbf2d047f624f92becd7c64168587278937b2cc
zyl <zyl971006@gmail.com>
2022-06-20 22:31:57 +0800

    Add minimal page

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-import imp
 from flask import Flask
+from flask import escape,url_for
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'welcome to my watchlist'
+    return '<h1>hello watchlist<h1><img src="http://helloflask.com/totoro.gif">'
+
+@app.route('/user/<name>')
+def user_page(name):
+    return f'User:{escape(name)}'
+
+@app.route('/test')
+def test_url_for():
+    print(url_for('hello'))
+    print(url_for('user_page',name='zyl'))
+    print(url_for('user_page',name='peter'))
+    print(url_for('test_url_for'))
+    print(url_for('test_url_for',num=2))
+    return 'Test page'
